97f42b24cef2cb558c83bf57fc1f9059e541fcd9
Tewodros <tyimers@gmail.com>
2022-05-05 15:07:19 -0400

    second commit with more features addes

--- script.js
+++ script.js
@@ -1,90 +1,89 @@
 var myPoints = 100;
 
-function updateHealthPoints(points) {
+function updateMyPoints(points) {
 
-	myPoints = points;
-	var healthBar = document.querySelector("#myHealthStatus");
-	healthBar.style.width = points + "%";
+    myPoints = points;
+    var statusBar = document.querySelector("#statusBar");
+    statusBar.style.width = points + "%";
 
-	if(myPoints < 1) {
-		alert("Game over!");
-		window.location.reload();
-	}
+    if(myPoints < 1) {
+        alert("Game over!");
+        window.location.reload();
+    }
 
 }
 
+//assigning background music and gunshot sounds
+var myGunSound = new Audio('laser_sound.mp3');
+var enemyGunSound = new Audio('enemy_sound.mp3');
+var music = new Audio("backMusic.mp3")
 
-function livingEnemies() {
-	return document.querySelectorAll(".enemy:not(.dead)");
+music.loop = true;
+
+//creating a function that identify how many livinig enemies that attacks me
+function activeEnemies() {
+    return document.querySelectorAll(".enemy:not(.dead)");
 }
-
 
 function Shoot(enemy) {
-	enemy.classList.add("dead");
+    enemy.classList.add("dead");
 
-	if(!livingEnemies().length) {
-		alert("You win!");
-		window.location.reload();
-	}
-
-}
-
-
-function enemyAttacksMe(enemy) {
-
-	if(myPoints > 0) {
-
-		enemy.classList.add("showing");
-
-		setTimeout(()=> {
-			enemyShootsMe(enemy);
-		}, 1000);
-
-		setTimeout(()=> {
-			enemy.classList.remove("showing");
-		}, 3000);
-		
-	}
-
+    if(!activeEnemies().length) {
+        alert("You win!");
+        window.location.reload();
+    }
 
 }
+//when enemy attacks me 
+function enemyAttack(enemy) {
+//check for my points 
+    if(myPoints > 0) {
 
+        enemy.classList.add("showing");
 
-function enemyShootsMe(enemy) {
+        setTimeout(()=> {
+            enemyShoots(enemy);
+        }, 1000);// enemy shoots after 1 second
 
-	if(!enemy.classList.contains("dead")) {
-
-		enemy.classList.add("shooting");
-		updateHealthPoints(myPoints - 20);
-
-		setTimeout(()=> {
-			enemy.classList.remove("shooting");
-		}, 200);
-
-	}
-
-}
-
-
-function randomEnemyAttacks() {
-
-	var randomEnemyNo = Math.random() * livingEnemies().length;
-	randomEnemyNo = Math.floor(randomEnemyNo);
-	var enemy = livingEnemies()[randomEnemyNo];
-
-	var randomDelay = Math.random() * 2000 + 1000;
-
-	setTimeout( ()=> {
-		enemyAttacksMe(enemy);
-		randomEnemyAttacks();
-	}, randomDelay);
+        setTimeout(()=> {
+            enemy.classList.remove("showing");
+        }, 3000);// enemy hides after 3 seconds of showing
+        
+    }
 
 }
+// enemy shooting at me 
+function enemyShoots(enemy) {
+	enemyGunSound.play();
+    if(!enemy.classList.contains("dead")) {
 
+        enemy.classList.add("shooting");
+        updateMyPoints(myPoints - 20);
 
+        setTimeout(()=> {
+            enemy.classList.remove("shooting");
+        }, 200);
 
+    }
 
+}
+//unexpected attack from my enemies
+function randomAttacks() {
 
+    var randomEnemyNo = Math.random() * activeEnemies().length;
+    randomEnemyNo = Math.floor(randomEnemyNo);
+    var enemy = activeEnemies()[randomEnemyNo];
 
+    var randomDelay = Math.random() * 2000 + 1000;
 
+    setTimeout( ()=> {
+        enemyAttack(enemy);
+        randomAttacks();
+    }, randomDelay);// displays a random attacks between 1 & 3 seconds in a random delay
 
+}
+function newGame() {
+    randomAttacks()
+    document.querySelector("button").style.display = "none";
+	music.play();
+}
